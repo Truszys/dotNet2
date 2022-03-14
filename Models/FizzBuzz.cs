142aac5b1257d639acf9c010ae54cec52eca56ba
Jakub Kondrat <Truszys@gmail.com>
2022-03-14 15:13:31 +0100

    zadanie bez stylowania

---
+++ Models/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotNet2.Models
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "Twój szczęśliwy numerek"), Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public int? Number { get; set; }
+        public (string, string) getOutput()
+        {
+            string outputMessage = "", outputClass = "";
+            if (Number % 3 == 0)
+            {
+                outputMessage += "Fizz";
+                outputClass = "success";
+            }
+            if (Number % 5 == 0)
+            {
+                outputMessage += "Buzz";
+                outputClass = "success";
+            }
+            if (outputMessage == "")
+            {
+                outputMessage = "Liczba: " + Number + " nie spełnia kryteriów FizzBuzz";
+                outputClass = "warning";
+            }
+            return (outputMessage, outputClass);
+        }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using dotNet2.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace dotNet2.Pages
@@ -6,15 +7,24 @@ namespace dotNet2.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+        [BindProperty(SupportsGet = true), FromQuery]
+        public string? Name { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnPost()
         {
-
+            if (FizzBuzz.Number == null)
+            {
+                return Page();
+            }
+            (ViewData["Message"], ViewData["MessageClass"]) = FizzBuzz.getOutput();
+            return Page();
         }
     }
 }
